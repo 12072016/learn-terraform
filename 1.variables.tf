bc5b0e0ddcee5b332ee76f26d3712f58c5eda067
neeli <neeliveti.kirankumar@gmai.com>
2023-08-05 22:35:02 +0530

    roboshop-terraform-variables

---
+++ 1.variables.tf
@@ -0,0 +1,24 @@
+#####shell scripting equivalent sample_string="hellow worls"
+### shell script we will use "echo"
+##### echo $sample_string
+###but#
+terraform
+    output "sample_string" {
+  value = var.sample_string
+}
+
+### here var. is nothing but $
+
+
+### above case we are just printing just variable,  if variable is a combinations of some string then following is the syantax for the same.
+###${} is a mandatory if we include the  variable inside string
+#example:
+
+output "sample_string" {
+  value = "value of sample_string = ${var.sample_string}"
+}
+
+
+variable "sample_string" {
+  default= "hellow World"
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # learn-terraform
+
+1.variables
+2.functions
+3.Data source
+4.resource
+5.Module
+6.locals
+7.provider block
+8.state file management
+9.loops
+10.conditions
